4cc3986db5880aad4dde586c464989aa177f0113
Marcello Masili <marcello.masili@gmail.com>
2018-10-22 16:43:21 +0200

    Commit prima su repository

--- PCFANsFirmware/src/main.cpp
+++ PCFANsFirmware/src/main.cpp
@@ -6,9 +6,9 @@ Schetch di test sulle ventole per PC
 */
 
 // PIN a cui è collegato il cavo + che alimenta la ventola 1
-#define FAN1_PIN             10
+#define INPUT_FAN_PIN             10
 // PIN a cui è collegato il cavo + che alimenta la ventola 2
-#define FAN2_PIN             11
+#define OUTPUT_FAN_PIN            11
 
 // La dichiarazione delle funzioni non è necessaria nel file .ino
 
@@ -23,8 +23,8 @@ void setup()
     Serial.println( "--- Sto inizializzando la scheda Arduino" );
 
 	// Configurazione iniziale delle ventole
-    PCFANSetup( FAN1_PIN );
-	PCFANSetup( FAN2_PIN );
+    PCFANSetup( INPUT_FAN_PIN );
+	PCFANSetup( OUTPUT_FAN_PIN );
 
     Serial.println( "--- Ho finito di inizializzare la scheda Arduino" );
 }
@@ -34,29 +34,29 @@ void loop()
 {
     Serial.println( "--- Inizio il loop principale della scheda Arduino" );
 
-    Serial.println( "Accendo ventola 1 alla velocità di defalt" );
+    Serial.println( "Accendo ventola di input alla velocità di defalt" );
 
     // Accendo la ventola 1 alla velocità di defalt
-    PCFANRotate( FAN1_PIN );
+    PCFANRotate( INPUT_FAN_PIN );
 
 	delay( 10000 );
 
-    Serial.println( "Spengo la ventola 1" );
+    Serial.println( "Spengo la ventola di input" );
 
     // Spengo la ventola 1
-	PCFANStop( FAN1_PIN );
+	PCFANStop( INPUT_FAN_PIN );
 
-	Serial.println( "Accendo ventola 2 alla velocità definita" );
+	Serial.println( "Accendo ventola di output alla velocità definita" );
 
     // Accendo la ventola 1 alla velocità definita
-    PCFANRotate( FAN2_PIN, FAN_MANUAL_MODE, 1000 );
+    PCFANRotate( OUTPUT_FAN_PIN, FAN_MANUAL_MODE, 1000 );
 
 	delay( 10000 );
 
-    Serial.println( "Spengo la ventola 2" );
+    Serial.println( "Spengo la ventola di output" );
 
     // Spengo la ventola 2
-	PCFANStop( FAN2_PIN );
+	PCFANStop( OUTPUT_FAN_PIN );
 }
 
 void DebugSetup()
